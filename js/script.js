aac415f8103d5fa328e81f3169a9e0b001d54323
YangKo <91419894+YangkoStudio@users.noreply.github.com>
2023-12-30 01:20:00 +0800

    1111

--- js/script.js
+++ js/script.js
@@ -13,3 +13,26 @@
 function about(){
     alert("Yangko Studio Website\nVersion:1000");
 }
+function startTime()
+{
+	var today=new Date()
+	var y=today.getFullYear()
+	var mon=today.getMonth()+1
+	var d=today.getDate()
+	var hour=today.getHours()
+	var min=today.getMinutes()
+	var sec=today.getSeconds()
+	var weeks = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+	var w= weeks[today.getDay()]
+	min=checkTime(min)
+	sec=checkTime(sec)
+	document.getElementById('txt').innerHTML=y+" 年 "+mon+" 月 "+d+" 日  "+w+" "+hour+":"+min+":"+sec
+	t=setTimeout('startTime()',500)
+}
+
+function checkTime(i)
+{
+	if (i<10)
+	{i="0" + i}
+	return i
+}
