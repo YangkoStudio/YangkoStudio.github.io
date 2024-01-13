20a1cae8964dfa096461d7b17403b19cac7cb00b
YangKo <91419894+YangkoStudio@users.noreply.github.com>
2024-01-13 12:51:34 +0800

    Delete app/www directory

--- app/www/js/plugins/Moji_outline.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-    var _Window_Base_createContents = Window_Base.prototype.createContents;
-    Window_Base.prototype.createContents = function() {
-        _Window_Base_createContents.apply(this, arguments);
-        this.contents.outlineColor = 'black';
-        this.contents.outlineWidth = 3;
-    };
-})();
