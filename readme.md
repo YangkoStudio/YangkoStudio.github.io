3bac54a0b640c9a9198bd644584fa941496e622f
YangKo <91419894+YangkoStudio@users.noreply.github.com>
2024-01-20 16:37:17 +0800

    1

--- readme.md
+++
@@ -1,1 +0,0 @@
-Yangko Studio
---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+function startTime()
+{
+	var today=new Date()
+	var y=today.getFullYear()
+	var mon=today.getMonth()+1
+	var d=today.getDate()
+	var hour=today.getHours()
+	var min=today.getMinutes()
+	var sec=today.getSeconds()
+	var weeks = new Array("Sun.", "Mon.", "Tues.", "Wed.", "Thur.", "Fri.", "Sat.");
+	var w= weeks[today.getDay()]
+	min=checkTime(min)
+	sec=checkTime(sec)
+	document.getElementById('txt').innerHTML=y+"-"+mon+"-"+d+" "+w+" "+hour+":"+min+":"+sec
+	t=setTimeout('startTime()',500)
+}
+function checkTime(i)
+{
+	if (i<10)
+	{i="0" + i}
+	return i
+}
