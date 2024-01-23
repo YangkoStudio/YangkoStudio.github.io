6eafd30a3fd21c8d7d8022c23ccc9e01f6f9a0ca
YangKo <91419894+YangkoStudio@users.noreply.github.com>
2024-01-23 23:34:55 +0800

    111

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+// Yangko Studio
+// config.js
+// text/javascript
+var version = "1.0.0"
--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,10 @@
-function startTime()
-{
-	var today=new Date()
-	var y=today.getFullYear()
-	var mon=today.getMonth()+1
-	var d=today.getDate()
-	var hour=today.getHours()
-	var min=today.getMinutes()
-	var sec=today.getSeconds()
-	var weeks = new Array("周日", "周一", "周二", "周三", "周四", "周五", "周六");
-	var w= weeks[today.getDay()]
-	min=checkTime(min)
-	sec=checkTime(sec)
-	document.getElementById('txt').innerHTML=y+"-"+mon+"-"+d+" "+w+" "+hour+":"+min+":"+sec
-	t=setTimeout('startTime()',500)
-}
-function checkTime(i)
-{
-	if (i<10)
-	{i="0" + i}
-	return i
-}
+//移动端适配
+if (isMobile()) {
+	document.getElementsById("mobile").style.display="none";
+} else {}
 
-alert("测试网站，版本号：1.1.0")
+//时间函数
+function startTime(){var today=new Date();var y=today.getFullYear();var mon=today.getMonth()+1;var d=today.getDate();var hour=today.getHours();var min=today.getMinutes();var sec=today.getSeconds();var weeks = new Array("周日", "周一", "周二", "周三", "周四", "周五", "周六");var w= weeks[today.getDay()];min=checkTime(min);sec=checkTime(sec);document.getElementById('txt').innerHTML=y+"-"+mon+"-"+d+" "+w+" "+hour+":"+min+":"+sec;t=setTimeout('startTime()',500)}
+function checkTime(i){if(i<10){i="0"+i}return i}
+//平台函数
+function isMobile(){let flag = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);return flag;}
