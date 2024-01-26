5e6344a1506a7e98c969e3ee1f61dc3533073c77
YangKo <91419894+YangkoStudio@users.noreply.github.com>
2024-01-26 18:45:42 +0800

    1.3.2

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,4 @@ if(isMobile()){document.write('<link rel="stylesheet" href="/src/css/mobile.css"
 function startTime(){var today=new Date();var y=today.getFullYear();var mon=today.getMonth()+1;var d=today.getDate();var hour=today.getHours();var min=today.getMinutes();var sec=today.getSeconds();var weeks=new Array("周日", "周一", "周二", "周三", "周四", "周五", "周六");var w=weeks[today.getDay()];min=checkTime(min);sec=checkTime(sec);document.getElementById('txt').innerHTML=y+"-"+mon+"-"+d+" "+w+" "+hour+":"+min+":"+sec;t=setTimeout('startTime()',500)}
 function checkTime(i){if(i<10){i="0"+i}return i}
 function isMobile(){let flag=/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);return flag;}
-function cookie(){
-  
-}
+function cookie(){}
